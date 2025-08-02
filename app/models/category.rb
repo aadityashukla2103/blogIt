b7b7e7b5bb55d016a7eb1e3333a314eab4709c4f
Aaditya Shukla <itsaadityashukla@gmail.com>
2025-08-03 03:25:49 +0530

    Added tests for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@
 
 class Category < ApplicationRecord
   has_and_belongs_to_many :posts
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
+# test/models/category_test.rb
+
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = build(:category)
+  end
+
+  test "should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = ""
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    create(:category, name: "Tech")
+    duplicate = build(:category, name: "Tech")
+    assert_not duplicate.valid?
+  end
 end
